9832731aa6448a0d738c32a1943117ef3237b1fb
Rafael Marinho Gonçalves Ferreira <rafael.mgf2001@gmail.com>
2020-08-06 17:17:23 -0300

    Criação e estilização do Header para as outras páginas e compartilhamento para o TeacherForm e TeacherList usando propriedades e interface

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
+import PageHeader from '../../components/PageHeader';
+
+import './styles.css';
+
 function TeacherForm() {
     return (
-        <h1>Teacher Form</h1>
+        <div id="page-teacher-form" className="container">
+            <PageHeader title="Que bom que você quer dar aulas!" />
+        </div>
     )
 }
 
