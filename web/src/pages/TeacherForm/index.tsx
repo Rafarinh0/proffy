d8f5aa4c9cb8ab39380190ef67f80c5f9a773e8a
Rafael Marinho Gonçalves Ferreira <rafael.mgf2001@gmail.com>
2020-08-08 17:57:12 -0300

    Fim da TeacherForm

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input/';
 
+import warningIcon from '../../assets/images/icons/warning.svg';
+
 import './styles.css';
 
 function TeacherForm() {
@@ -28,9 +30,18 @@ function TeacherForm() {
 
                     <Input name="subject" label="Matéria"></Input>
                     <Input name="cost" label="Custo da hora/aula"></Input>
-                    <Input name="whatsapp" label="WhatsApp"></Input>
-
                 </fieldset>
+
+                <footer>
+                    <p>
+                        <img src={warningIcon} alt="Aviso importante" />
+                        Importante! <br />
+                        Preencha todos os dados
+                    </p>
+                    <button type="button">
+                        Salvar cadastro
+                    </button>
+                </footer>
             </main>
         </div>
     )
