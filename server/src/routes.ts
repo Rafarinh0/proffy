5ae090b844b152877d3c4664510c25724f604768
Rafael Marinho Gonçalves Ferreira <rafael.mgf2001@gmail.com>
2020-08-07 21:20:38 -0300

    Rota de listagem de conexões e adição de CORS

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -9,7 +9,9 @@ const connectionsController = new ConnectionsController();
 routes.get('/classes', classesController.index);
 routes.post('/classes', classesController.create);
 
+routes.get('/connections', connectionsController.index);
 routes.post('/connections', connectionsController.create);
+
 
 
 export default routes;
